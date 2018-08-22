2cf5c3760a6b06fa9ff8868432b3efc5878cebf3
Deepa Verma <deverma@synapseco.com>
2018-08-22 14:58:53 +0530

    user stats

--- wpcivicrmuserstats.php
+++ wpcivicrmuserstats.php
@@ -46,33 +46,100 @@ class wpcivicrmuserstats_Widget extends WP_Widget {
         (int) $current_userID,
         'employer_id'
     );
-    $employee_list = array();
-    $employee_list[] = CRM_Core_DAO::getFieldValue('CRM_Contact_DAO_Contact',
-        (int) $currentEmployer,
-        'id', 
-        'employer_id'
-    );
-    $employees = implode(", ",$employee_list);
-    $clauses = array();
-    //where clause for contact id and participant status(regsitered or attended)
-    $clauses[] = "civicrm_participant.contact_id IN ($employees)";
-    $clauses[] = "civicrm_participant.status_id IN ( 1, 2 )";
-    
-    if ( isset($instance['event_type_id']) ) {
-      $event_id = $instance['event_type_id'];
-      $clauses[] = "civicrm_event.id IN ($event_id)";
+    $employee_list = civicrm_api3('Contact', 'get', array(
+      'sequential' => 1,
+      'employer_id' => $currentEmployer,
+    ));
+    $employeeID = array();
+    if(!empty($employee_list['values']) ){
+      foreach($employee_list['values'] as $data) {
+        $employeeID[] = CRM_Utils_Array::value('contact_id', $data);
+      }
     }
-    if ( isset($instance['event_year']) ) {
+   
+    //get event_type_id
+    $event_id = array();
+    if ( !empty($instance['event_type_id']) ) {
+      $events = $instance['event_type_id'];
+      $event_id = explode(",",$events);
+    }
+    else{
+      $params = array(
+        'sequential' => TRUE,
+      );
+      $allevents = civicrm_api3('Event', 'get', $params);
+      if(!empty($allevents['values']) ){
+        foreach($allevents['values'] as $data) {
+          $event_id[] = CRM_Utils_Array::value('id', $data);
+        }
+      }   
+    }
+    
+    //event_year
+    $eventsID = array();
+    $final_event_id = array();
+    if ( !empty($instance['event_year']) ) {
       $date = $instance['event_year'];
-      $clauses[] = "DATE_FORMAT(civicrm_event.start_date, '%Y') <= $date AND DATE_FORMAT(civicrm_event.end_date, '%Y') >= $date";
+      $event_start_date = $date."-01-01";
+      $event_end_date = $date."-12-31";
+      
+      $events_params = array(
+        'event_start_date' => array(
+            'BETWEEN' => array(
+              '0' => $event_start_date,
+              '1' => $event_end_date,
+            ), 
+        )    
+      );
+      try{
+        $eventsList = civicrm_api3('Event', 'get', $events_params);
+      }
+      catch (CiviCRM_API3_Exception $e) {
+        // Handle error here.
+        $errorMessage = $e->getMessage();
+        $errorCode = $e->getErrorCode();
+        $errorData = $e->getExtraParams();
+        return array(
+          'is_error' => 1,
+          'error_message' => $errorMessage,
+          'error_code' => $errorCode,
+          'error_data' => $errorData,
+        );
+      }
+      
+      if(!empty($eventsList['values']) ){
+        foreach($eventsList['values'] as $data) {
+          $eventsID[] = CRM_Utils_Array::value('id', $data);
+        }
+      }      
     }
-    
-    $whereClause = !empty($clauses) ? implode(' AND ', $clauses) : '(1)';
-    $query = "SELECT civicrm_participant.id FROM civicrm_participant INNER JOIN civicrm_event ON civicrm_participant.event_id = civicrm_event.id WHERE $whereClause";
-    // execute query
-    $dao = CRM_Core_DAO::executeQuery($query);
-    $count = $dao->N;
-    echo 'user stats '.$count; 
+    if( !empty($event_id) && !empty($eventsID) ){
+      $final_event_id = array_intersect($event_id, $eventsID);
+    }
+    else if( !empty($event_id) && empty($eventsID) ){
+      $final_event_id = $event_id;
+    }
+    $params = array(
+      'contact_id' => $employeeID,
+      'participant_status_id' => array( 1, 2 ),
+      'event_id' => $final_event_id,
+    );
+    try{
+      $result = civicrm_api3('Participant', 'getcount', $params);
+    }
+    catch (CiviCRM_API3_Exception $e) {
+      // Handle error here.
+      $errorMessage = $e->getMessage();
+      $errorCode = $e->getErrorCode();
+      $errorData = $e->getExtraParams();
+      return array(
+        'is_error' => 1,
+        'error_message' => $errorMessage,
+        'error_code' => $errorCode,
+        'error_data' => $errorData,
+      );
+    }
+    echo 'user stats '.$result; 
   }
 
 	// output the option form field in admin Widgets screen
@@ -88,6 +155,13 @@ class wpcivicrmuserstats_Widget extends WP_Widget {
     <p>
       <label for="<?php echo $this->get_field_id( 'event_year' ); ?>">Event Year:</label>
       <input type="text" id="<?php echo $this->get_field_id( 'event_year' ); ?>" name="<?php echo $this->get_field_name( 'event_year' ); ?>" value="<?php echo esc_attr( $event_year ); ?>" />
+    </p>
+    
+    <?php 
+    $style = ! empty( $instance['style'] ) ? $instance['style'] : ''; ?>
+    <p>
+      <label for="<?php echo $this->get_field_id( 'style' ); ?>">Stats Style:</label>
+      <input type="text" id="<?php echo $this->get_field_id( 'style' ); ?>" name="<?php echo $this->get_field_name( 'style' ); ?>" value="<?php echo esc_attr( $style ); ?>" />
     </p><?php 
   }
 
@@ -96,6 +170,7 @@ class wpcivicrmuserstats_Widget extends WP_Widget {
     $instance = $old_instance;
     $instance[ 'event_type_id' ] = strip_tags( $new_instance[ 'event_type_id' ] );
     $instance[ 'event_year' ] = strip_tags( $new_instance[ 'event_year' ] );
+    $instance[ 'style' ] = strip_tags( $new_instance[ 'style' ] );
     return $instance;
   }
 }
@@ -110,6 +185,7 @@ function wpcivicrmuserstats_widget_shortcode( $atts ) {
           'stat'  => '',
           'event_type_id'  => '',
           'event_year' => '',
+          'style' => '',
       ), 
       $atts 
   ));
